b8bb38b00495f20546acb9018403c49c1194d867
Ronaldo <ronaldomorales.ai@gmail.com>
2026-02-10 15:51:04 -0500

    added a checkResult function that gives a point to player or computer and add the point to the score

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 const button = document.querySelectorAll('.btn')
+const playerScore = document.querySelector('.player')
+const computerScore = document.querySelector('.computer')
+const p = document.querySelector('p')
 
 button.forEach(element => {
     element.addEventListener('click', playGame)
 })
 
 async function playGame() {
+    let playerChoose = event.target.value
     const computer = 'bot'
     const res = await fetch(`/api?bot=${computer}`)
     const data = await res.json()
 
-    console.log(data)
+    checkResult(playerChoose, data.computer)
+}
+
+function checkResult(player, computer) {
+    console.log(player)
+    console.log(computer)
+
+    if (player === 'rock' && computer === 'scissor' || player === 'paper' && computer === 'rock' || player === 'scissor' && computer === 'paper') {
+        p.textContent = 'POINT PLAYER'
+        playerScore.textContent = Number(playerScore.textContent) + 1
+    } else if (computer === 'rock' && player === 'scissor' || computer === 'paper' && player === 'rock' || computer === 'scissor' && player === 'paper') {
+        p.textContent = 'POINT COMPUTER'
+        computerScore.textContent = Number(computerScore.textContent) + 1
+    } else {
+        p.textContent = 'DRAW NO POINT'
+    }
 }
